13e77b4f8530b047bc2244ace4c4f5ec5bd488fa
darren <darren.miao@e-graphics.com>
2014-01-17 17:47:33 +0800

    a

--- www.dogwin.net/application/controllers/welcome.php
+++ www.dogwin.net/application/controllers/welcome.php
@@ -19,7 +19,37 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		//$this->load->view('welcome_message');
+		print_r($this->shorturl('https://www.facebook.com/dogwin.net/app_464202623696627'));
+	}
+	function shorturl($input) {
+		$base32 = array (
+				'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+				'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+				'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+				'y', 'z', '0', '1', '2', '3', '4', '5'
+		);
+	
+		$hex = md5($input);
+		$hexLen = strlen($hex);
+		$subHexLen = $hexLen / 8;
+		$output = array();
+	
+		for ($i = 0; $i < $subHexLen; $i++) {
+			$subHex = substr ($hex, $i * 8, 8);
+			$int = 0x3FFFFFFF & (1 * ('0x'.$subHex));
+			$out = '';
+	
+			for ($j = 0; $j < 6; $j++) {
+				$val = 0x0000001F & $int;
+				$out .= $base32[$val];
+				$int = $int >> 5;
+			}
+	
+			$output[] = $out;
+		}
+	
+		return $output;
 	}
 }
 
